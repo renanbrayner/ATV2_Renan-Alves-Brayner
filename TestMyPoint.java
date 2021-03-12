b0f88555b1f5fea703619d9b17225372e5828481
Renan Brayner <renanbrayner@gmail.com>
2021-03-11 23:22:43 -0300

    Created MyPoint tests

---
+++ TestMyPoint.java
@@ -0,0 +1,153 @@
+public class TestMyPoint {
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+
+	public static void main() {
+		System.out.println("Running all MyPoint tests...");
+		testConstructorWithoutParams();
+		testConstructorWithParams();
+		testGetX();
+		testGetY();
+		testSetX();
+		testSetY();
+		testSetXY();
+		testToString();
+		testDistanceTwoParams();
+		testDistanceOneParam();
+		testEquals();
+		System.out.println("Done! \n");
+	}
+
+	public static void testConstructorWithoutParams() {
+		MyPoint testPoint = new MyPoint();
+
+		if (testPoint instanceof MyPoint) {
+			System.out.println(ANSI_GREEN + "✓ Passed testConstructorWithoutParams" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testConstructorWithoutParams" + ANSI_RESET);
+		}
+	}
+
+	public static void testConstructorWithParams() {
+		MyPoint testPoint = new MyPoint(1, 2);
+
+		if (testPoint instanceof MyPoint && testPoint.getX() == 1 && testPoint.getY() == 2) {
+			System.out.println(ANSI_GREEN + "✓ Passed testConstructorWithParams" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testConstructorWithParams" + ANSI_RESET);
+		}
+	}
+
+	public static void testGetX() {
+		MyPoint testPoint = new MyPoint();
+
+		if (testPoint.getX() == 0) {
+			System.out.println(ANSI_GREEN + "✓ Passed testGetX" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testGetX" + ANSI_RESET);
+		}
+	}
+
+	public static void testGetY() {
+		MyPoint testPoint = new MyPoint();
+
+		if (testPoint.getY() == 0) {
+			System.out.println(ANSI_GREEN + "✓ Passed testGetY" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testGetY" + ANSI_RESET);
+		}
+	}
+	
+	public static void testSetX() {
+		MyPoint testPoint = new MyPoint();
+
+		testPoint.setX(1);
+
+		if (testPoint.getX() == 1) {
+			System.out.println(ANSI_GREEN + "✓ Passed testSetX" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testSetX" + ANSI_RESET);
+		}
+	}
+
+	public static void testSetY() {
+		MyPoint testPoint = new MyPoint();
+
+		testPoint.setY(1);
+
+		if (testPoint.getY() == 1) {
+			System.out.println(ANSI_GREEN + "✓ Passed testSetY" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testSetY" + ANSI_RESET);
+		}
+	}
+
+	public static void testSetXY() {
+		MyPoint testPoint = new MyPoint();
+
+		testPoint.setXY(1, 2);
+
+		if (testPoint.getX() == 1 && testPoint.getY() == 2) {
+			System.out.println(ANSI_GREEN + "✓ Passed testSetXY" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testSetXY" + ANSI_RESET);
+		}
+	}
+
+	public static void testToString() {
+		MyPoint testPoint = new MyPoint();
+
+		String result = testPoint.toString();
+		String expectedResult = "MyPoint (0,0)";
+
+		if (result.equals(expectedResult)) {
+			System.out.println(ANSI_GREEN + "✓ Passed testToString" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testToString" + ANSI_RESET);
+			System.out.println("   Expected: " + expectedResult + "\n   Got: " + result);
+		}
+	}
+
+	public static void testDistanceTwoParams() {
+		MyPoint testPoint = new MyPoint();
+
+		double result = testPoint.distance(0, 1);
+		double expectedResult = 1.0;
+
+		if (result == expectedResult) {
+			System.out.println(ANSI_GREEN + "✓ Passed testDistanceTwoParams" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testDistanceTwoParams" + ANSI_RESET);
+			System.out.println("   Expected: " + expectedResult + "\n   Got: " + result);
+		}
+	}
+
+	public static void testDistanceOneParam() {
+		MyPoint testPoint = new MyPoint();
+		MyPoint testPoint2 = new MyPoint(0, 1);
+
+		double result = testPoint.distance(testPoint2);
+		double expectedResult = 1.0;
+
+
+		if (result == expectedResult) {
+			System.out.println(ANSI_GREEN + "✓ Passed testDistanceOneParam" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testDistanceOneParam" + ANSI_RESET);
+			System.out.println("   Expected: " + expectedResult + "\n   Got: " + result);
+		}
+	}
+
+	public static void testEquals() {
+		MyPoint testPoint = new MyPoint();
+		MyPoint testPoint2 = new MyPoint();
+
+		if (testPoint.equals(testPoint2)) {
+			System.out.println(ANSI_GREEN + "✓ Passed testEquals" + ANSI_RESET);
+		} else {
+			System.out.println(ANSI_RED + "✗ Failed testEquals" + ANSI_RESET);
+		}
+	}
+}
+
